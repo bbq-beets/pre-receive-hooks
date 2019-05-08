1d4fcd95ce41b9c608a30b95d1f91c5a91b43f61
Peter Wagner <git@mycloudand.me>
2019-05-08 15:52:04 -0400

    bad pwd assumption

--- demo.sh
+++ demo.sh
@@ -2,8 +2,9 @@
 
 BUF=$(</dev/stdin)
 
+DIR=$(dirname $0)
 if echo $BUF | grep -q "debug$"; then
-  echo $BUF | ./dump-environment.sh
+  echo $BUF | ${DIR}/dump-environment.sh
 else
-  echo $BUF | ./require-jira-issue.sh
+  echo $BUF | ${DIR}/require-jira-issue.sh
 fi
