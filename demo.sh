5333aab86497e7714b10fecc4d92587ab816958b
Peter Wagner <git@mycloudand.me>
2019-05-08 16:05:22 -0400

    change debug branch name

--- demo.sh
+++ demo.sh
@@ -5,7 +5,7 @@ set -ex
 BUF=$(cat)
 
 DIR=$(dirname $0)
-if echo $BUF | grep -q "debug$"; then
+if echo $BUF | grep -q "test$"; then
   echo $BUF | ${DIR}/dump-environment.sh
 else
   echo $BUF | ${DIR}/require-jira-issue.sh
